40d1da18294e6812bc6542555b763405f9e4518f
Mateusz Domanski <mateuszsev@gmail.com>
2021-05-14 16:25:33 +0200

    'init'

---
+++ precache-manifest.712d3f5620d8527243530608af9cd9bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "750d2cb81e7575d0a3b7dbb9cd2eb1be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c201a93700ae191a7201",
+    "url": "/static/css/2.04486c29.chunk.css"
+  },
+  {
+    "revision": "abf9687b47e6c43c6a50",
+    "url": "/static/css/main.2db6ba37.chunk.css"
+  },
+  {
+    "revision": "c201a93700ae191a7201",
+    "url": "/static/js/2.b77112c6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b77112c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf9687b47e6c43c6a50",
+    "url": "/static/js/main.06ebc649.chunk.js"
+  },
+  {
+    "revision": "3bb02338304888f7e826",
+    "url": "/static/js/runtime-main.cee7b8e8.js"
+  }
+]);
